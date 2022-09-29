028484164ef9e03053f1007ad1d06686a62e6545
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2022-09-29 19:51:47 +0530

    Updated EMployee in Identity

--- WorkflowAuto/Data/ApplicationDbContext.cs
+++ WorkflowAuto/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WorkflowAuto.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
