152367591c72077af9dc6dbcfe4ca73e45eded43
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2022-10-05 18:03:53 +0530

    Added Employee Controller and Index Page

--- WorkflowAuto/Models/EmployeeListVM.cs
+++ WorkflowAuto/Models/EmployeeListVM.cs
@@ -1,10 +1,20 @@
-﻿namespace WorkflowAuto.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkflowAuto.Models
 {
     public class EmployeeListVM
     {
         public string Id { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
         public DateTime DateJoined { get; set; }
 
     }
